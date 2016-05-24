40a88d00a0349b570040b1f866e74c71a1b15df9
Tinywave Netsphere <xyoc@hotmail.de>
2016-05-24 20:09:21 +0200

    restructured code, moved bot-functions out of gamepedia client

--- faeriawikibot.py
+++ faeriawikibot.py
@@ -8,6 +8,7 @@ import gamepedia_client
 import os, sys
 import configparser
 import re
+import datetime
 
 class Faeriawikibot:
     merlinlist = []
@@ -62,10 +63,6 @@ class Faeriawikibot:
         return self.cardlist
 
     def handle_unupdated_cards(self, card):
-        if card['card_id'] == '295':
-            card['life'] = 2
-            card['power'] = 2
-            card['desc'] = '{charge|2} Gain +2/+2 each time you attack a god while in your deck or hand.'
         if card['card_id'] == '331':
             card['card_name'] = 'Wisdom (Special)'
         if card['card_id'] == '320':
@@ -119,16 +116,142 @@ class Faeriawikibot:
     def create_action_templates(self):
         if self.gc is None:
             self.create_gamepedia_client()
-        self.gc.create_action_templates()
+        actions = [['ranged_attack', 'Ranged'], ['charge', 'Charge'], ['gift', 'Gift'], ['production', 'Production'],
+                   ['combat', 'Combat'], ['protector', 'Protection'], ['taunt', 'Taunt'], ['haste', 'Haste'],
+                   ['last_words', 'Last Words'], ['deathtouch', 'Deathtouch'], ['aquatic', 'Aquatic'], ['jump', 'Jump'],
+                   ['flying', 'Flying'], ['activate', 'Activate'], ['options', 'Choose one:'], ['faeria', 'Faeria'], ['random', 'random']]
+        for action in actions:
+            page = self.gc.read('Template:{0}'.format(action[0]))
+            if page.text() == '':
+                page.save('[[' + action[0] + '|' + action[1] + ' {{#if: {{{1|}}}|{{{1|}}} }}]]',
+                          summary='Initialized with default configuration')
 
     '''
-    Update cards
+    Create/Update card on wiki
+    Move conflicting old cards to '/{old card name}_(Historical)'
     '''
     def update_cards(self):
         if self.gc is None:
             self.create_gamepedia_client()
         for x in range(0, len(self.merlinlist) - 1):
-            self.gc.submit_card(self.merlinlist[x], self.cardlist[x])
+            mc = self.merlinlist[x]
+            card = self.cardlist[x]
+            page = self.gc.read(mc['card_name'])
+            text = page.text()
+            if '{{Card infobox' in text:
+                page = self.gc.read(mc['card_name'] + ' (Historical)')
+                page.save('{{historical content}}\n' + text)
+
+            page = self.gc.read(mc['card_name'])
+            if '{{Card stats' in text:
+                try:
+                    uc = self.update_card(text, card)
+                    page.save(uc)
+                except ValueError as e:
+                    print('ValueError on \n{text}'.format(text=text))
+            else:
+                page.save(card)
+
+    '''
+    Select and replace (=> update) the 'Card stats' template instance
+    '''
+
+    def update_card(self, text, card):
+        if '== Changelog ==' not in text:
+            text += '\n\n== Changelog ==\n{{changelog\n|height = 200\n|content = {{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}\n}}'
+        start, end = self.textregion_selector(text, '{{Card stats', '{', '}')
+        oldtext = text[start:end + 1]
+        olddict = self.card2dict(oldtext)
+        newdict = self.card2dict(card)
+        changelog = '{{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}\n{{patch|' + str(
+            datetime.date.today().isocalendar()[0]) + '|' + str(datetime.date.today().isocalendar()[1]) + '}}:\n'
+        if olddict != newdict:
+            for key in olddict.keys():
+                try:
+                    if olddict[key] != newdict[key]:
+                        changelog += self.generate_changelogitem(key, olddict[key], newdict[key])
+                except KeyError:
+                    pass
+            print('******************')
+            print(olddict['card_name'])
+            print(changelog)
+            return str(text).replace(oldtext, card).replace(
+               '{{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}', changelog)
+        return str(text).replace(oldtext, card)
+
+    def generate_changelogitem(self, key, oldvalue, newvalue):
+        if oldvalue is None:
+            oldvalue = ''
+        if newvalue is None:
+            newvalue = ''
+        changelogitem = '* {key} changed from "{old}" to "{new}"\n'
+        if key == 'card_color':
+            return '* {{cl_color|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'card_name':
+            return '* {{cl_name|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'card_type':
+            return '* {{cl_type|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'rarity':
+            return '* {{cl_rarity|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'faeria':
+            return '* {{cl_faeria|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'lake':
+            return '* {{cl_lake|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'desert':
+            return '* {{cl_desert|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'mountain':
+            return '* {{cl_mountain|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'forest':
+            return '* {{cl_forest|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'power':
+            return '* {{cl_power|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'life':
+            return '* {{cl_life|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'desc':
+            return '* {{cl_desc|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'codexcode1':
+            return '* {{cl_codexcode1|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'codexcode2':
+            return '* {{cl_codexcode2|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key == 'codexcode3':
+            return '* {{cl_codexcode3|' + oldvalue + '|' + newvalue + '}}\n'
+        elif key not in ['ability1', 'ability2', 'ability3', 'ability4', 'ability5', 'illustration']:
+            return '* {{cl_unknown|' + oldvalue + '|' + newvalue + '}}\n'
+        else:
+            return ''
+
+    def card2dict(self, card):
+        cardsplit = card.split('\n')
+        carddict = {}
+        for line in cardsplit:
+            try:
+                key, value = line.split('=', 1)
+                key = key.split('|')[1]
+                key = key.strip()
+                value = value.strip()
+                carddict[key] = value
+            except ValueError:
+                pass
+        return carddict
+
+    '''
+    Select region of the 'Card stats' template which should get replaced with the updated card
+    '''
+
+    @staticmethod
+    def textregion_selector(text, starttext, increase, decrease):
+        text = str(text)
+        startmarker = text.find(starttext)
+        level = 0
+        for x in range(startmarker, len(text)):
+            if text[x] == increase:
+                level += 1
+            elif text[x] == decrease:
+                level -= 1
+                if level == 0:
+                    return startmarker, x
+
+        raise ValueError
 
     '''
     Add one '{'/'}' to each '{'/'}' to make gamepedia link to the templates
@@ -143,6 +266,7 @@ class Faeriawikibot:
     '''
     Replaces referenced cards in description with actual mediawiki links
     '''
+    @staticmethod
     def handle_english_description_links(self, description):
         resultlist = re.findall('<b>.*?<\/b>',description)
         for result in resultlist:
--- gamepedia_client.py
+++ gamepedia_client.py
@@ -1,5 +1,4 @@
 import mwclient
-import datetime
 
 
 class GamepediaClient:
@@ -37,21 +36,6 @@ class GamepediaClient:
         page.save(text)
 
     '''
-    Create action template which later link to tags in the card description.
-    '''
-
-    def create_action_templates(self):
-        actions = [['ranged_attack', 'Ranged'], ['charge', 'Charge'], ['gift', 'Gift'], ['production', 'Production'],
-                   ['combat', 'Combat'], ['protector', 'Protection'], ['taunt', 'Taunt'], ['haste', 'Haste'],
-                   ['last_words', 'Last Words'], ['deathtouch', 'Deathtouch'], ['aquatic', 'Aquatic'], ['jump', 'Jump'],
-                   ['flying', 'Flying'], ['activate', 'Activate'], ['options', 'Choose one:'], ['faeria', 'Faeria'], ['random', 'random']]
-        for action in actions:
-            page = self.mwc.Pages['Template:{0}'.format(action[0])]
-            if page.text() == '':
-                page.save('[[' + action[0] + '|' + action[1] + ' {{#if: {{{1|}}}|{{{1|}}} }}]]',
-                          summary='Initialized with default configuration')
-
-    '''
     Upload local image to wiki
     '''
 
@@ -65,125 +49,4 @@ class GamepediaClient:
     def upload_remote_image(self, url, destination, description):
         self.mwc.upload(url=url, filename=destination, description=description)
 
-    '''
-    Create/Update card on wiki
-    Move conflicting old cards to '/{old card name}_(Historical)'
-    '''
 
-    def submit_card(self, mc, card):
-        page = self.mwc.Pages[mc['card_name']]
-        text = page.text()
-        if '{{Card infobox' in text:
-            page = self.mwc.Pages[mc['card_name'] + ' (Historical)']
-            page.save('{{historical content}}\n' + text)
-
-        page = self.mwc.Pages[mc['card_name']]
-        if '{{Card stats' in text:
-            try:
-                uc = self.update_card(text, card)
-                page.save(uc)
-            except ValueError as e:
-                print('ValueError on \n{text}'.format(text=text))
-        else:
-            page.save(card)
-
-    '''
-    Select and replace (=> update) the 'Card stats' template instance
-    '''
-
-    def update_card(self, text, card):
-        if '== Changelog ==' not in text:
-            text += '\n\n== Changelog ==\n{{changelog\n|height = 200\n|content = {{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}\n}}'
-        start, end = self.textregion_selector(text, '{{Card stats', '{', '}')
-        oldtext = text[start:end + 1]
-        olddict = self.card2dict(oldtext)
-        newdict = self.card2dict(card)
-        changelog = '{{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}\n{{patch|' + str(
-            datetime.date.today().isocalendar()[0]) + '|' + str(datetime.date.today().isocalendar()[1]) + '}}:\n'
-        if olddict != newdict:
-            for key in olddict.keys():
-                try:
-                    if olddict[key] != newdict[key]:
-                        changelog += self.generate_changelogitem(key, olddict[key], newdict[key])
-                except KeyError:
-                    pass
-            print('******************')
-            print(olddict['card_name'])
-            print(changelog)
-            return str(text).replace(oldtext, card).replace(
-               '{{empty|DO NOT REMOVE OR EDIT THIS OTHERWISE CHANGELOG UPDATE BREAKS}}', changelog)
-        return str(text).replace(oldtext, card)
-
-    def generate_changelogitem(self, key, oldvalue, newvalue):
-        if oldvalue is None:
-            oldvalue = ''
-        if newvalue is None:
-            newvalue = ''
-        changelogitem = '* {key} changed from "{old}" to "{new}"\n'
-        if key == 'card_color':
-            return '* {{cl_color|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'card_name':
-            return '* {{cl_name|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'card_type':
-            return '* {{cl_type|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'rarity':
-            return '* {{cl_rarity|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'faeria':
-            return '* {{cl_faeria|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'lake':
-            return '* {{cl_lake|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'desert':
-            return '* {{cl_desert|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'mountain':
-            return '* {{cl_mountain|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'forest':
-            return '* {{cl_forest|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'power':
-            return '* {{cl_power|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'life':
-            return '* {{cl_life|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'desc':
-            return '* {{cl_desc|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'codexcode1':
-            return '* {{cl_codexcode1|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'codexcode2':
-            return '* {{cl_codexcode2|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key == 'codexcode3':
-            return '* {{cl_codexcode3|' + oldvalue + '|' + newvalue + '}}\n'
-        elif key not in ['ability1', 'ability2', 'ability3', 'ability4', 'ability5', 'illustration']:
-            return '* {{cl_unknown|' + oldvalue + '|' + newvalue + '}}\n'
-        else:
-            return ''
-
-    def card2dict(self, card):
-        cardsplit = card.split('\n')
-        carddict = {}
-        for line in cardsplit:
-            try:
-                key, value = line.split('=', 1)
-                key = key.split('|')[1]
-                key = key.strip()
-                value = value.strip()
-                carddict[key] = value
-            except ValueError:
-                pass
-        return carddict
-
-    '''
-    Select region of the 'Card stats' template which should get replaced with the updated card
-    '''
-
-    @staticmethod
-    def textregion_selector(text, starttext, increase, decrease):
-        text = str(text)
-        startmarker = text.find(starttext)
-        level = 0
-        for x in range(startmarker, len(text)):
-            if text[x] == increase:
-                level += 1
-            elif text[x] == decrease:
-                level -= 1
-                if level == 0:
-                    return startmarker, x
-
-        raise ValueError
